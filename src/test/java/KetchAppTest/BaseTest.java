65054a7bb1e9d1e6e7cb81c5a120cd02a0b32146
patelharish <patelharish304@gmail.com>
2025-02-14 12:22:30 +0530

    added basetest

---
+++ src/test/java/KetchAppTest/BaseTest.java
@@ -0,0 +1,46 @@
+package KetchAppTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+
+
+public class BaseTest {
+	
+	public AndroidDriver driver;
+	public AppiumDriverLocalService service;
+	
+	@BeforeTest
+	public void setUp() throws MalformedURLException {
+		// run appium server here optional
+		/*service = new AppiumServiceBuilder()
+				.withAppiumJS(
+						new File("C://Users//admin//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		service.start();*/
+
+		// AndroidDriver;
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setPlatformName("Android");
+		options.setAutomationName("UiAutomator2");
+		options.setDeviceName("motorola edge 50 pro");
+		options.setAppPackage("com.ketchapp");
+		// com.ketchapp.MainActivity - Ketch
+		options.setAppActivity(".MainActivity");
+		// options.setAppActivity("com.android.vending.AssetBrowserActivity");
+
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+	}
+
+	@AfterTest
+	public void tearDown() {
+        //driver.quit();
+       //service.stop();
+	}
+}
---
+++ src/test/java/KetchAppTest/SerachTest.java
@@ -0,0 +1,24 @@
+package KetchAppTest;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class SerachTest extends BaseTest {
+
+	@Test
+	public void AppiumFirst() throws InterruptedException {
+
+		// supported locators: xpath,id,className,accessbilityid,androiduiautomator
+		driver.findElement(By.xpath(
+				"//android.widget.Button[@resource-id=\"com.android.permissioncontroller:id/permission_allow_button\"]"))
+				.click();
+		//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		//click on global serach
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.ImageView")).click();
+		
+		//driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.ImageView\").instance(1)")).click();
+		//driver.findElement(By.xpath("//android.widget.EditText[@text=\"Search\"]")).sendKeys("tshirts");
+	}
+
+}
